3748fa2ba5d1e78c938e165b60e13199d6e8d62e
Tony Do <dvhbkhn@gmail.com>
2023-04-23 08:40:22 +0900

    second commit

--- git.sh
+++ git.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git pull
 git add .
-git commit -m $1
+git commit -m "$1"
 git branch -M main
-git remote add origin https://github.com/dovanhuong/epson_printer.git
+#git remote add origin https://github.com/dovanhuong/epson_printer.git
 git push -u origin main
 echo "completed commit...\n"
